8e4f18634f5cfe5f4aba6183af27d6ff40ce7f21
JamesSzg <jamesszg@hotmail.com>
2021-05-22 19:07:44 +0800

    nevbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,111 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+    <el-radio-button :label="false">展开</el-radio-button>
+    <el-radio-button :label="true">收起</el-radio-button>
+  </el-radio-group>
+  <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
+    :collapse="isCollapse">
+    <el-submenu index="1">
+      <template #title>
+        <i class="el-icon-location"></i>
+        <span>导航一</span>
+      </template>
+      <el-menu-item-group>
+        <template #title>分组一</template>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-2">选项2</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="分组2">
+        <el-menu-item index="1-3">选项3</el-menu-item>
+      </el-menu-item-group>
+      <el-submenu index="1-4">
+        <template #title>选项4</template>
+        <el-menu-item index="1-4-1">选项1</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+    <el-menu-item index="2">
+      <i class="el-icon-menu"></i>
+      <template #title>导航二</template>
+    </el-menu-item>
+    <el-menu-item index="3" disabled>
+      <i class="el-icon-document"></i>
+      <template #title>导航三</template>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <i class="el-icon-setting"></i>
+      <template #title>导航四</template>
+    </el-menu-item>
+  </el-menu>
 </template>
 
-<style lang="scss">
+<script>
+  export default {
+    data() {
+      return {
+        isCollapse: true,
+        nevSelected: null
+      };
+    },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style>
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+</style>
+
+
+<!-- <template>
+  <img src="./assets/logo.png">
+  <div>
+    <p>
+      提供 el-fade-in-linear 和 el-fade-in 两种效果。
+    </p>
+
+    <el-button type="primary" @click="show = !show">渐变</el-button>
+    <div style="display: flex; margin-top: 20px; height: 100px;">
+      <transition name='el-fade-in-linear'>
+        <div v-show='show' class="transition-box">
+          true
+        </div>
+      </transition>
+    </div>
+  </div>
+
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data: () => ({
+    show: true
+  })
+}
+</script>
+
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</style> -->
--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import installElementPlus from './plugins/element'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(ElementPlus).use(store).use(router).mount("#app");
